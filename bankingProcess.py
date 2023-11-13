f20dd7d6ce112549e5a41cf9e2bd532949e1de79
Yeasir Arafat <yeasir@cdda.io>
2023-11-13 15:11:28 +0600

    FEAT sending money done

--- bankingProcess.py
+++ bankingProcess.py
@@ -1,6 +1,6 @@
 import sys
 from InquirerPy import inquirer
-from cls import file
+from cls import send_money,change_pass
 
 def bankingProcess (user,data):
     print("Welcome to the banking system", user["name"])
@@ -10,44 +10,33 @@ def bankingProcess (user,data):
         ).execute()
     confirm = inquirer.confirm(message="Confirm?").execute()
     if(confirm):
-        if(operation == "check Balance"):
+        if(operation == "Check Balance"):
             print("Your Balance is", user["balance"])
             previous_operation = inquirer.confirm(message="Do you want to back previous menu?").execute()
             if(previous_operation):
-                bankingProcess(user)
+                bankingProcess(user,data)
             else:
                 sys.exit()
         elif(operation == "Change Password"):
-            old_password = inquirer.secret(message="Enter your old password:").execute()
-            new_password = inquirer.secret(message="Enter your new password:").execute()
-            confirm_password = inquirer.secret(message="Confirm your new password:").execute()
-            if(old_password == user["password"] and new_password == confirm_password):
-                
-                user["password"] = new_password
-                for i in data:
-                    if i["phone"] == user["phone"]:
-                         i["password"] = new_password
-                    file().write_data(data)
-                print("Password changed successfully")
+            if change_pass(user):
                 previous_operation = inquirer.confirm(message="Do you want to back previous menu?").execute()
                 if(previous_operation):
                     bankingProcess(user,data)
                 else:
                     sys.exit()
-            else:
-                print("check your old password and maybe your password and confirm password are not same")
+            
         elif(operation == "send Money"):
-            receiver_phone = inquirer.text(message="Enter the receiver Phone number:").execute()
-            amount = inquirer.text(message="Enter Amount").execute()
-            if(amount > user["balance"]):
-                print("Insufficient Balance")
-                confirm= inquirer.confirm(message="Do you want to try again?").execute()
-                if confirm:
-                   amount = inquirer.text(message="Enter Amount").execute()
-            for i in data:
-                if i["phone"] != receiver_phone:
-                    print("Receiver User not found. Please check the phone number")
-                    receiver_phone = inquirer.text(message="Enter the receiver Phone number again:").execute()
+            if send_money(user):
+                print("Sending Money is Successful")
+            else:
+                print("Something went wrong")
+                confirm = inquirer.confirm(message="Do you want to try again?").execute()
+                if(confirm):
+                    send_money(user)
+                else:
+                    sys.exit()
+        elif(operation == "Exit"):
+            sys.exit()
             
             
                     
--- cls.py
+++ cls.py
@@ -1,4 +1,5 @@
 import json
+from InquirerPy import inquirer
 
 
 class file:
@@ -10,9 +11,54 @@ class file:
             f.write(json.dumps(_new_data, indent=4))
 
 
-def send_money (receiver_phone, sender_phone,amount):
+def send_money ( user):
+    amount = get_amount(user)
+    receiver_phone = get_receiver()
+    sender_phone = user["phone"]
+    data = file().get_data()
     try :
-        print("Sss")
+        for i in data:
+            if i["phone"] == sender_phone:
+                i["balance"] =i["balance"] - amount
+            elif i["phone"] == receiver_phone:
+                i["balance"] = i["balance"] + amount
+        file().write_data(data)
+        return True        
+    except Exception as error:
+        print(error,"error")
+        return False   
 
-    except e as e:
-        print("Ss")   
+def get_receiver ():
+    number = inquirer.text(message="Enter the receiver Phone number:").execute()
+    data = file().get_data()
+    for i in data:
+        if i["phone"] == number :
+            return number
+    
+    print("Receiver User not found. Please check the phone number")
+    get_receiver()
+
+
+def get_amount (user):
+    amount = int(inquirer.text(message="Enter Amount").execute())
+    if int(user["balance"]) > amount :
+            return amount
+    else:
+        print("Insufficient Balance Check the input")
+        get_amount(user)
+
+
+def change_pass (user):
+    old_password = inquirer.secret(message="Enter your old password:").execute()
+    new_password = inquirer.secret(message="Enter your new password:").execute()
+    confirm_password = inquirer.secret(message="Confirm your new password:").execute()
+    data = file().get_data()
+    if(old_password == user["password"] and new_password == confirm_password):
+        for i in data:
+            if i["phone"] == user["phone"]:
+                    i["password"] = new_password
+            file().write_data(data)
+        return True
+    else:
+        print("check your old password and maybe your password and confirm password are not same")
+        change_pass(user)
